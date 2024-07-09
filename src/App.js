3dfce4f8527994edba0ed2a1b7060dc3a5eea042
Stephen Djaba Jnr <65154229+KingDjaba@users.noreply.github.com>
2024-07-09 15:44:00 +0000

    Upda

--- src/App.js
+++ src/App.js
@@ -6,13 +6,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App(props) {
     return (
         <div>
-            <BrowserRouter>
+          <BrowserRouter>
           <Routes>
-          <Route path="/" element={<Home/>} />
-
-              
+          <Route path="/" element={<Home/>} />   
           </Routes>
-        
       </BrowserRouter>
         </div>
     );
